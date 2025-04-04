5f2f2d132cffeeab7b2eb1c29c5a3cd5cb2ab6c9
kyhlsd <kmyghn@gmail.com>
2025-04-04 17:02:21 +0900

    Feat: 라벨 줄 넘김 글자 단위

--- Modules/Shared/UIComponents/Sources/PrayRequestContentTableViewCell.swift
+++ Modules/Shared/UIComponents/Sources/PrayRequestContentTableViewCell.swift
@@ -23,8 +23,7 @@ class PrayRequestContentTableViewCell: UITableViewCell {
         label.font = .systemFont(ofSize: 14)
         label.numberOfLines = 2
         label.lineBreakMode = .byTruncatingTail
-        label.setContentHuggingPriority(.required, for: .vertical)
-        label.setContentCompressionResistancePriority(.required, for: .vertical)
+        label.lineBreakStrategy = .pushOut
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
