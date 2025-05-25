422c5bd6f06c866291bb2a6372339f62cc77814f
kyhlsd <kmyghn@gmail.com>
2025-05-25 19:31:24 +0900

    Refactor: PrayEditorView - final, 접근 제어자, view let 선언, 함수 위치 순서 조정

--- Modules/Features/PrayRequest/Sources/PrayEditorView.swift
+++ Modules/Features/PrayRequest/Sources/PrayEditorView.swift
@@ -9,80 +9,22 @@ import UIKit
 import Core
 import Shared
 
+// 입력 상태에 따라 NavigationBar RightButton 상태 Update
+protocol RightBarButtonStateDelegate: AnyObject {
+    func updateRightBarButtonEnabled()
+}
+
 final class PrayEditorView: UIView {
     
-    private let titleLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont(name: "IropkeBatangM", size: 14)
-        let subjectStrokeTextAttributes: [NSAttributedString.Key: Any] = [
-            .strokeWidth: -2.0
-        ]
-        label.attributedText = NSAttributedString(
-            string: "제목",
-            attributes: subjectStrokeTextAttributes
-        )
-        label.numberOfLines = 1
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private let setupTitleLabel() {
-        
-    }
-    
-    private let titleTextField: PaddedTextField = {
-        let textField = PaddedTextField()
-        textField.font = UIFont(name: "IropkeBatangM", size: 16)
-        let placeHolderStrokeTextAttributes: [NSAttributedString.Key: Any] = [
-            .strokeColor: UIColor.lightGray,
-            .foregroundColor: UIColor.lightGray,
-        ]
-        textField.attributedPlaceholder = NSAttributedString(
-            string: PlaceholderStrings.prayTitleInputPlaceholder,
-            attributes: placeHolderStrokeTextAttributes
-        )
-        textField.layer.borderColor = UIColor.lightGray.cgColor
-        textField.layer.borderWidth = 1
-        textField.layer.cornerRadius = 8
-        
-        textField.translatesAutoresizingMaskIntoConstraints = false
-        return textField
-    }()
-    
-    private let prayContentLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont(name: "IropkeBatangM", size: 14)
-        let subjectStrokeTextAttributes: [NSAttributedString.Key: Any] = [
-            .strokeWidth: -2.0
-        ]
-        label.attributedText = NSAttributedString(
-            string: "내용",
-            attributes: subjectStrokeTextAttributes
-        )
-        label.numberOfLines = 1
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-    
-    private let prayContentTextView: UITextView = {
-        let textView = UITextView()
-        textView.text = PlaceholderStrings.prayContentInputPlaceholder
-        textView.textColor = .lightGray
-        textView.font = UIFont(name: "IropkeBatangM", size: 16)
-        textView.textContainerInset = .init(top: 8, left: 8, bottom: 8, right: 8)
-        textView.textContainer.lineFragmentPadding = 0
-        textView.layer.borderColor = UIColor.lightGray.cgColor
-        textView.layer.borderWidth = 1
-        textView.layer.cornerRadius = 8
-        
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        return textView
-    }()
+    private let titleLabel = UILabel()
+    private let titleTextField = PaddedTextField()
+    private let prayContentLabel = UILabel()
+    private let prayContentTextView = UITextView()
     
     private var prayContentTextViewBottomConstraint: NSLayoutConstraint!
-    public weak var delegate: RightBarButtonStateDelegate?
+    weak var delegate: RightBarButtonStateDelegate?
     
-    public var isAllTextsValid: Bool = false {
+    var isAllTextsValid: Bool = false {
         didSet {
             delegate?.updateRightBarButtonEnabled()
         }
@@ -90,10 +32,9 @@ final class PrayEditorView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        setup()
         
-        titleTextField.delegate = self
-        prayContentTextView.delegate = self
+        setup()
+        setupDelegate()
         
         titleTextField.addTarget(self, action: #selector(titleTextFieldDidChange(_:)), for: .editingChanged)
     }
@@ -103,10 +44,20 @@ final class PrayEditorView: UIView {
     }
     
     private func setup() {
+        setupTitleLabel()
+        setupTitleTextField()
+        setupPrayContentLabel()
+        setupPrayContentTextView()
+        
         addSubview(titleLabel)
         addSubview(titleTextField)
         addSubview(prayContentLabel)
         addSubview(prayContentTextView)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
+        prayContentLabel.translatesAutoresizingMaskIntoConstraints = false
+        prayContentTextView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor),
@@ -130,7 +81,62 @@ final class PrayEditorView: UIView {
         prayContentTextViewBottomConstraint.isActive = true
     }
     
-    public func checkTextsValidation(titleText: String?, contentText: String?) {
+    private func setupTitleLabel() {
+        titleLabel.font = UIFont(name: "IropkeBatangM", size: 14)
+        let subjectStrokeTextAttributes: [NSAttributedString.Key: Any] = [
+            .strokeWidth: -2.0
+        ]
+        titleLabel.attributedText = NSAttributedString(
+            string: "제목",
+            attributes: subjectStrokeTextAttributes
+        )
+        titleLabel.numberOfLines = 1
+    }
+    
+    private func setupTitleTextField() {
+        titleTextField.font = UIFont(name: "IropkeBatangM", size: 16)
+        let placeHolderStrokeTextAttributes: [NSAttributedString.Key: Any] = [
+            .strokeColor: UIColor.lightGray,
+            .foregroundColor: UIColor.lightGray,
+        ]
+        titleTextField.attributedPlaceholder = NSAttributedString(
+            string: PlaceholderStrings.prayTitleInputPlaceholder,
+            attributes: placeHolderStrokeTextAttributes
+        )
+        titleTextField.layer.borderColor = UIColor.lightGray.cgColor
+        titleTextField.layer.borderWidth = 1
+        titleTextField.layer.cornerRadius = 8
+    }
+    
+    private func setupPrayContentLabel() {
+        prayContentLabel.font = UIFont(name: "IropkeBatangM", size: 14)
+        let subjectStrokeTextAttributes: [NSAttributedString.Key: Any] = [
+            .strokeWidth: -2.0
+        ]
+        prayContentLabel.attributedText = NSAttributedString(
+            string: "내용",
+            attributes: subjectStrokeTextAttributes
+        )
+        prayContentLabel.numberOfLines = 1
+    }
+    
+    private func setupPrayContentTextView() {
+        prayContentTextView.text = PlaceholderStrings.prayContentInputPlaceholder
+        prayContentTextView.textColor = .lightGray
+        prayContentTextView.font = UIFont(name: "IropkeBatangM", size: 16)
+        prayContentTextView.textContainerInset = .init(top: 8, left: 8, bottom: 8, right: 8)
+        prayContentTextView.textContainer.lineFragmentPadding = 0
+        prayContentTextView.layer.borderColor = UIColor.lightGray.cgColor
+        prayContentTextView.layer.borderWidth = 1
+        prayContentTextView.layer.cornerRadius = 8
+    }
+    
+    private func setupDelegate() {
+        titleTextField.delegate = self
+        prayContentTextView.delegate = self
+    }
+    
+    private func checkTextsValidation(titleText: String?, contentText: String?) {
         guard let titleText = titleText, let contentText = contentText else {
             isAllTextsValid = false
             return
@@ -151,7 +157,7 @@ final class PrayEditorView: UIView {
     }
     
     // TODO: Util로 빼기
-    public func convertToPrayRequestContent(with text: String) -> [PrayRequestContent] {
+    func convertToPrayRequestContent(with text: String) -> [PrayRequestContent] {
         var results = [PrayRequestContent]()
         
         // 입력 끝에 개행 추가 (마지막 항목까지 매치되도록)
@@ -188,7 +194,7 @@ final class PrayEditorView: UIView {
         return results
     }
     
-    public func convertFromPrayRequestContents(with contents: [PrayRequestContent]) -> String {
+    func convertFromPrayRequestContents(with contents: [PrayRequestContent]) -> String {
         let lines = contents.map { content in
             if content.subject.isEmpty {
                 return content.description
@@ -200,11 +206,13 @@ final class PrayEditorView: UIView {
         return lines.joined(separator: "\n\n")
     }
     
-    public func getPrayContentText() -> String {
-        return prayContentTextView.text
+    func configure(with prayRequest: PrayRequest) {
+        titleTextField.text = prayRequest.title
+        prayContentTextView.text = convertFromPrayRequestContents(with: prayRequest.contents)
+        prayContentTextView.textColor = .black
     }
     
-    public func updateBottomConstraint(with constant: CGFloat = 0.0, animationDuration: TimeInterval) {
+    func updateBottomConstraint(with constant: CGFloat = 0.0, animationDuration: TimeInterval) {
         prayContentTextViewBottomConstraint.constant = -12 - constant
 
         UIView.animate(withDuration: animationDuration) {
@@ -212,34 +220,32 @@ final class PrayEditorView: UIView {
         }
     }
     
-    public func configure(with prayRequest: PrayRequest) {
-        titleTextField.text = prayRequest.title
-        prayContentTextView.text = convertFromPrayRequestContents(with: prayRequest.contents)
-        prayContentTextView.textColor = .black
+    func getPrayContentText() -> String {
+        return prayContentTextView.text
     }
     
-    public func getEditedPrayRequest() -> PrayRequest {
+    func getEditedPrayRequest() -> PrayRequest {
         return PrayRequest(date: Date(), title: titleTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? "", contents: convertToPrayRequestContent(with: prayContentTextView.text))
     }
 }
 
 extension PrayEditorView: UITextViewDelegate {
     
-    public func textViewDidBeginEditing(_ textView: UITextView) {
+    func textViewDidBeginEditing(_ textView: UITextView) {
         if textView.text == PlaceholderStrings.prayContentInputPlaceholder {
             textView.text = ""
             textView.textColor = .black
         }
     }
     
-    public func textViewDidEndEditing(_ textView: UITextView) {
+    func textViewDidEndEditing(_ textView: UITextView) {
         if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
             textView.text = PlaceholderStrings.prayContentInputPlaceholder
             textView.textColor = .lightGray
         }
     }
     
-    public func textViewDidChange(_ textView: UITextView) {
+    func textViewDidChange(_ textView: UITextView) {
         checkTextsValidation(titleText: titleTextField.text, contentText: textView.text)
     }
 }
@@ -264,8 +270,4 @@ extension PrayEditorView: UITextFieldDelegate {
         checkTextsValidation(titleText: textField.text, contentText: prayContentTextView.text)
         delegate?.updateRightBarButtonEnabled()
     }
-}
-
-public protocol RightBarButtonStateDelegate: AnyObject {
-    func updateRightBarButtonEnabled()
 }
